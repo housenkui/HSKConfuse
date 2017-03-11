f83165d41ebc7aebfa4377b7d6738a7ed045cb7c
Daves_Code_Hou <840737320@qq.com>
2017-03-11 13:37:51 +0800

    Initial Commit

---
+++ HSKConfuse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HSKConfuse
+//
+//  Created by Code_Hou on 2017/3/11.
+//  Copyright © 2017年 侯森魁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HSKConfuse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HSKConfuse
+//
+//  Created by Code_Hou on 2017/3/11.
+//  Copyright © 2017年 侯森魁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
