44bc3f16a607f46b702485b025f0e9251edb4361
Daves_Code_Hou <840737320@qq.com>
2017-03-16 20:27:02 +0800

    来自  简书 “猜猜我叫什么_bd1f” 的技术支持，只匹配以“hsk_”字符串前缀的方法名，而不是匹配字符‘h’ 、‘s’、‘k’、‘_’

--- HSKConfuse/Resource/confuse.sh
+++ HSKConfuse/Resource/confuse.sh
@@ -11,7 +11,8 @@ HEAD_FILE="$PROJECT_DIR/HSKConfuse/Resource/codeObfuscation.h"
 export LC_CTYPE=C
 
 #取以.m或.h结尾的文件以+号或-号开头的行 |去掉所有+号或－号|用空格代替符号|n个空格跟着<号 替换成 <号|开头不能是IBAction|用空格split字串取第二部分|排序|去重复|删除空行|删掉以init开头的行>写进func.list
-grep -h -r -I  "^[-+]" $CONFUSE_FILE  --include '*.[mh]' |sed "s/[+-]//g"|sed "s/[();,: *\^\/\{]/ /g"|sed "s/[ ]*</</"| sed "/^[ ]*IBAction/d"|awk '{split($0,b," "); print b[2]; }'| sort|uniq |sed "/^$/d"|sed "/^[^hsk_]/d" >$STRING_SYMBOL_FILE
+grep -h -r -I  "^[-+]" $CONFUSE_FILE  --include '*.[mh]' |sed "s/[+-]//g"|sed "s/[();,: *\^\/\{]/ /g"|sed "s/[ ]*</</"| sed "/^[ ]*IBAction/d"|awk '{split($0,b," "); print b[2]; }'| sort|uniq |sed "/^$/d"|sed -n "/^hsk_/p" >$STRING_SYMBOL_FILE
+
 
 #维护数据库方便日后作排重,一下代码来自念茜的微博
 createTable()
--- HSKConfuse/ViewController.m
+++ HSKConfuse/ViewController.m
@@ -18,10 +18,26 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)Hsk_Function2{
+}
 -(void)hsk_Function2{
     
 }
-
+- (void)handXXX{
+    
+}
+- (void)hand{
+    
+}
+- (void)hsk_start{
+    
+}
+- (void)hfunction{
+    
+}
+- (void)_hou{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
